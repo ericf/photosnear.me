de4a2b02eff9310826bb58e665b0d2b4417dca9c
Eric Ferraiuolo <eferraiuolo@gmail.com>
2012-03-05 12:32:12 -0500

    Added iOS `orientationchange` fix.
    See: https://github.com/scottjehl/iOS-Orientationchange-Fix

--- public/js/config.js
+++ public/js/config.js
@@ -15,7 +15,8 @@ YUI.GlobalConfig = {
     gallery    : 'gallery-2011.10.20-23-28',
 
     modules: {
-        typekit: 'http://use.typekit.com/' + TYPEKIT_ID + '.js'
+        'typekit'   : 'http://use.typekit.com/' + TYPEKIT_ID + '.js',
+        'ios-oc-fix': '/vendor/ios-orientationchange-fix.js'
     },
 
     groups: {
