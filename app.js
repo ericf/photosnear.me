cb45257e8a2ba25cacca75e094b7c9c8bda4e115
Eric Ferraiuolo <eferraiuolo@gmail.com>
2012-04-20 12:30:30 -0400

    WIP PhotosNearMe server.

---
+++ app.js
@@ -0,0 +1,91 @@
+require('./conf/config');
+
+var connect = require('connect'),
+    combo   = require('combohandler'),
+    express = require('express'),
+    Y       = require('yui').use('handlebars'),
+
+    app    = express.createServer(),
+    pubDir = global.config.pubDir;
+
+// -- Express config -----------------------------------------------------------
+app.configure('development', function () {
+    // Gives us pretty logs in development. Must run before other middleware.
+    app.use(express.logger(
+        '[:date] :req[x-forwarded-for] ":method :url" :status [:response-time ms]'
+    ));
+});
+
+app.configure(function () {
+    // Don't ignore trailing slashes in routes.
+    app.set('strict routing', true);
+
+    // Use our custom Handlebars-based view engine as the default.
+    app.register('.handlebars', require('./lib/view'));
+    app.set('view engine', 'handlebars');
+
+    // Local values that will be shared across all views. Locals specified at
+    // render time will override these values if they share the same name.
+    app.set('view options', Y.merge(require('./conf/common'), {
+        config: global.config
+    }));
+
+    // Middleware.
+    app.use(app.router);
+    app.use(express.favicon());
+    app.use(express.static(pubDir));
+});
+
+app.configure('development', function () {
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack     : true
+    }));
+});
+
+app.configure('production', function () {
+    app.enable('view cache');
+    app.use(express.errorHandler());
+});
+
+// -- Routes -------------------------------------------------------------------
+
+// Root.
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+// Combo-handler for JavaScript.
+app.get('/combo', combo.combine({rootPath: pubDir + '/js'}), function (req, res) {
+    if (connect.utils.conditionalGET(req)) {
+        if (!connect.utils.modified(req, res)) {
+            return connect.utils.notModified(res);
+        }
+    }
+
+    res.send(res.body, 200);
+});
+
+// Dymanic resource for precompiled templates.
+app.get('/templates.js', (function () {
+    var precompiled = require('./lib/templates').precompiled,
+        templates   = [];
+
+    Y.Object.each(precompiled, function (template, name) {
+        templates.push({
+            name    : name,
+            template: template
+        });
+    });
+
+    return function (req, res) {
+        res.render('templates', {
+            layout   : false,
+            templates: templates
+        }, function (err, view) {
+            res.send(view, {'Content-Type': 'application/javascript'}, 200);
+        });
+    };
+}()));
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,42 +1,5 @@
-var connect = require('connect'),
-    combo   = require('combohandler'),
-    express = require('express'),
-
-    app    = express.createServer(),
-    port   = process.env.PORT || 3000,
-    pubDir = __dirname + '/public';
-
-app.configure(function () {
-    app.use(express.favicon());
-    app.use(express.staticCache());
-    app.use(express.static(pubDir));
-});
-
-// Combo-handler for JavaScript.
-app.get('/js', combo.combine({rootPath: pubDir + '/js'}), function (req, res) {
-    if (connect.utils.conditionalGET(req)) {
-        if (!connect.utils.modified(req, res)) {
-            return connect.utils.notModified(res);
-        }
-    }
-
-    res.send(res.body, 200);
-});
-
-// The actual PNM app.
-app.get('/', function (req, res) {
-    res.sendfile('index.html');
-});
-
-// Restrict to only known paths that the app can respond to:
-//
-//  - "/place/:id/"
-//  - "/photo/:id/"
-//
-// Redirects back to "/" with the URL as a fragment, e.g. "/#/photo/:id/"
-app.get(/^\/(?:(?:place|photo)\/\d+\/)$/, function (req, res) {
-    res.redirect('/#' + req.url, 302);
-});
+var app  = require('./app'),
+    port = process.env.PORT || 3000;
 
 app.listen(port, function () {
     console.log('Server listening on ' + port);
