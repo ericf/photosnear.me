b7a94b10bcdf5070b23921c5950daa49f0a71d15
Eric Ferraiuolo <eferraiuolo@gmail.com>
2012-04-28 18:14:39 -0400

    Support automatic place lookup from URL. /boston -> /places/2367105/
    This adds a catch-all route which takes the text and looks up the
    corresponding place that matches it the best. It then redirects to the
    /places/:id/ route where the request response will be rendered.

--- public/js/models/place.js
+++ public/js/models/place.js
@@ -12,6 +12,7 @@ Place = Y.Base.create('place', Y.Model, [Y.ModelSync.YQL], {
 
     queries: {
         placeFromId    : 'SELECT {attrs} FROM geo.places WHERE woeid={id}',
+        placeFromText  : 'SELECT {attrs} FROM geo.places WHERE text="{text}"',
         placeFromLatLon: 'SELECT {attrs} FROM geo.places WHERE woeid ' +
                             'IN (SELECT place.woeid FROM flickr.places ' +
                             'WHERE api_key={api_key} ' +
@@ -19,8 +20,15 @@ Place = Y.Base.create('place', Y.Model, [Y.ModelSync.YQL], {
                             'AND lon={longitude})'
     },
 
-    buildQuery: function () {
+    buildQuery: function (options) {
         if (this.isNew()) {
+            if (options.text) {
+                return Lang.sub(this.queries.placeFromText, {
+                    text : options.text,
+                    attrs: Place.YQL_ATTRS
+                });
+            }
+
             // assumes we at least have a lat/lon
             return Lang.sub(this.queries.placeFromLatLon, {
                 api_key  : FLICKR_API_KEY,
@@ -37,9 +45,10 @@ Place = Y.Base.create('place', Y.Model, [Y.ModelSync.YQL], {
     },
 
     parse: function (results) {
-        if ( ! results) { return; }
+        if (!results) { return; }
 
-        var data     = results.place,
+        var place    = results.place,
+            data     = Lang.isArray(place) ? place[0] : place,
             centroid = data.centroid,
             country  = data.country,
             region   = data.admin1,
