36d6747efee4eef098f53b79fd8330d74b861c92
Eric Ferraiuolo <eferraiuolo@gmail.com>
2012-05-09 16:07:41 -0400

    Move TypeKit into the HTML document's `<head>`.

--- conf/common.js
+++ conf/common.js
@@ -4,6 +4,7 @@ module.exports = {
     env        : config.env,
     layout     : config.layout,
     min        : config.env.production ? '-min' : '',
+    typekit    : config.typekit,
     yui_config : JSON.stringify(config.yui.client),
     yui_version: config.yui.version
 };
